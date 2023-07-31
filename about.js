783e5e0d7f9fa1a789dc3771af308077d2e4bff5
DHANUSBALA <118575109+DHANUSBALA@users.noreply.github.com>
2023-07-31 11:05:02 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,54 @@
+let myResume={
+    "basics": {
+      "name": "balamurugan M",
+      "email": "dhanushbala59@gmail.com",
+      "phone": 8056341911,
+      "degree": "B.E",
+      "location": {
+        "address": "15/1 keelaveliyour pillayarkovilpatti",
+        "postalCode": 621313,
+        "city": "karur",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    },
+    "work": [
+      {
+        "company": "ysi automotive pvt ltd",
+        "position": "Quality inspector",
+        "startDate": "2018-8-4",
+        "endDate": "2020-03-22",
+        "summary": "i am the checking  material parts for better quality ",
+      },
+    ],
+    "education": [
+      {
+        "institution": "shivani Engineering college",
+        "department": "Mechanical",
+        "studyType": "fulltime",
+        "batch start year": 2014,
+        "batch end year": 2018,
+        "gpa": 6.6,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+        "project": [
+          "automatic attendance maintaing system using js language"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,cricket,",
+      }
+    ]
+  }
+  console.log(myResume);
